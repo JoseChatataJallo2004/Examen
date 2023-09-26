c013fa09a41d86c220a8e12212c6a482a35bc658
kenned <jcvaldivieso96@gmail.com>
2023-09-25 22:01:57 -0500

    Primer commit

---
+++ src/main/java/org/examen/ExamenChatataApplication.java
@@ -0,0 +1,13 @@
+package org.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenChatataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenChatataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/examen/ExamenChatataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenChatataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
